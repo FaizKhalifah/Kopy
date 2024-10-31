c2f8cf7203dcd38db13c4ed00e543df9bd2257b5
Faiz Khalifah <faiz_doc@outlook.com>
2024-10-31 20:54:39 +0700

    url methods

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>hello<h1>"
+    return "<h1>hello<h1>", 200
+
+@app.route('/name', methods=['GET'])
+def name():
+    return "my name is faiz"
+
+#dynamic route
+@app.route('/greet/<name>')
+def greet(name):
+    return f"halo {name}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
--- readme.md
+++ readme.md
@@ -12,6 +12,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/') -> the url route
+
 def index():
     return "hello"
 
