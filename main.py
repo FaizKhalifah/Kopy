ac1448fe81fc58e26f4290f31e184709e7c2df8b
Faiz Khalifah <faiz_doc@outlook.com>
2024-10-31 21:28:16 +0700

    html template

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="views")
 
 @app.route('/')
 def index():
-    return "<h1>hello<h1>", 200
+    return render_template('index.html')
 
-@app.route('/name', methods=['GET'])
-def name():
-    return "my name is faiz"
-
-#dynamic route
-@app.route('/greet/<name>')
-def greet(name):
-    return f"halo {name}"
+@app.route('/nama',methods=["Get"])
+def nama():
+    nama1 = "Ravi"
+    nama2 = "Bagas"
+    nama3 = "Zufar"
+    listprodi = ["Teknik informatika", "teknik komputer", "teknologi informasi", "sistem informasi"]
+    return render_template('nama.html', nama1 = nama1, nama2 = nama2, nama3=nama3, listprodi=listprodi)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
