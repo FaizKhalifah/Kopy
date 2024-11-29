3394485bfd0765c3998bde23a55420cddf79440e
Faiz Khalifah <faiz_doc@outlook.com>
2024-11-29 08:08:51 +0700

    coffee model

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder="views")
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/nama',methods=["Get"])
-def nama():
-    nama1 = "Ravi"
-    nama2 = "Bagas"
-    nama3 = "Zufar"
-    listprodi = ["Teknik informatika", "teknik komputer", "teknologi informasi", "sistem informasi"]
-    return render_template('nama.html', nama1 = nama1, nama2 = nama2, nama3=nama3, listprodi=listprodi)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+db = SQLAlchemy()
+def create_app():
+    app = Flask(__name__)
+    
---
+++ models/coffee.py
@@ -0,0 +1,17 @@
+from . import db
+
+class Coffee(db.model) : 
+    __tablename__ = "Coffees"
+
+    id = db.column(db.Integer,primary_key=True)
+    name=db.column(db.String(100),nullable=False)
+    description = db.Column(db.String(200))
+    additional = db.Column(db.String(100))
+
+    def to_dict(self):
+        return{
+            'id':self.id,
+            'name':self.name,
+            'description':self.description,
+            'additional':self.additional
+        }
